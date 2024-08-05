9d283f4944bf6759bf16f83f2bb4c036cd12069c
Nilesh20-R <nilesh30j2009@gmail.com>
2024-08-05 19:20:25 +0530

    Add files via upload

---
+++ BinarySearch.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int low, int high, int x)
+{
+    while (low <= high) {
+        int mid = (low + high)/2;
+
+        if (arr[mid] == x)
+            return mid;
+
+        if (arr[mid] < x)
+            low = mid + 1;
+
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
+
+int main(void)
+{
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int x;
+    cout<<"Enter an Element : ";
+    cin>>x;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = binarySearch(arr, 0, n - 1, x);
+    if(result == -1) cout << "Element is not present in array";
+    else cout << "Element is present at index " << result;
+    return 0;
+}
