496e3061ac997bce59f12969d43b838fbb848756
Nilesh20-R <nilesh30j2009@gmail.com>
2024-11-17 16:49:27 +0530

    Add files via upload

---
+++ tfPermute.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+vector<string> solve(int k, int n, string current)
+{
+     vector<string> ans ;
+
+    // base case
+    if(k>n)
+    {
+        ans.push_back(current) ;
+        return ans;
+    }
+
+    else{
+          vector<string> resultT =  solve(k+1,n,current+'T') ;
+          ans.insert(ans.end(), resultT.begin(), resultT.end()) ;
+          vector<string> resultF= solve(k+1,n,current+'F') ;
+          ans.insert(ans.end(), resultF.begin(), resultF.end()) ;
+    }
+    return ans ;
+}
+
+int main()
+{
+    int n;
+    cout<<"enter n to make the different possible combinations of true and false: "<<endl;
+    cin>>n ;
+    vector<string> res= solve(1,n,"") ;
+    for (const string& step : res) 
+    {
+        cout << step << endl;
+    }
+
+    return 0;
+}
